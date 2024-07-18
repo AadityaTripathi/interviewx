66191cd5fcf95f1468fb62032e554328b22cd735
AadityaTripathi <tripathiaaditya02@gmail.com>
2024-07-19 00:18:51 +0530

    bug fix

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: "postgresql://InterviewX_owner:2YFJqk9exwvV@ep-dark-sound-a5kp0959.us-east-2.aws.neon.tech/InterviewX?sslmode=require",
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_UR,
     }
   };
