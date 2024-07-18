86171e5f9a5f7277d1305b36ae32d588f7b9c8bc
AadityaTripathi <tripathiaaditya02@gmail.com>
2024-07-19 02:49:08 +0530

    added add-interview Ui + setup google gemini api

--- app/dashboard/_components/NewInterview.jsx
+++ app/dashboard/_components/NewInterview.jsx
@@ -9,9 +9,64 @@ import {
     DialogTrigger,
   } from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button';
-  
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { chatSession } from '@/utils/GeminiAIModel';
+import { LoaderCircle } from 'lucide-react';
+import { db } from '@/utils/db';
+import { MockInterview } from '@/utils/schema';
+import { v4 as uuidv4} from 'uuid';
+import { useUser } from '@clerk/nextjs';
+import moment from 'moment/moment';
+
+
 export const NewInterview = () => {
     const [OpenDialog, setOpenDialog] = useState(false);
+    const [jobPosition, setJobPosition] = useState();
+    const [jobDesc, setJobDesc] = useState();
+    const [jobExperience, setJobExperience] = useState();
+    const [loadings, setLoadings] = useState(false);
+    const [jsonResponse, setJsonResponse] = useState([]);
+
+    const {user} = useUser();
+
+    const onSubmit = async(e) => {
+        setLoadings(true);
+        e.preventDefault();
+        console.log(jobDesc, jobPosition, jobExperience);
+
+        const Inputprompt = "Job Position: "+ jobPosition + ", Job description: "  + jobDesc + ", Years of experience: "+ jobExperience +", Depending on this information, generate 5 interview questions with answers in JSON formate. Give Question and Answered as fields in JSON. Do not add any unnecessary explanation in the response. just the json response";
+
+        const result = await chatSession.sendMessage(Inputprompt);
+        let mockjsonresponse = (result.response.text()).replace('```json', '').replace('```', '');
+        mockjsonresponse.slice(0,-4)
+        //console.log(mockjsonresponse);
+        setJsonResponse(mockjsonresponse);
+        console.log(JSON.parse(mockjsonresponse));
+
+        //stroing in db
+        if(mockjsonresponse){
+            const resp = await db.insert(MockInterview)
+            .values({
+                mockId:uuidv4(),
+                jsonMockResp: mockjsonresponse,
+                jobPosition: jobPosition,
+                jobDesc: jobDesc,
+                jobExperience: jobExperience,
+                createdBy: user?.primaryEmailAddress?.emailAddress,
+                createdAt: moment().format('DD-MM-yyyy'),
+            }).returning({mockId: MockInterview.mockId});
+
+            console.log("Intered id", resp);
+            if(resp){
+                setOpenDialog(false);
+            }
+        }else{
+            console.log("No response from Gemini AI");
+        }
+
+        setLoadings(false);
+    };
     return (
     <div>
         <div className="p-10 border rounded-lg bg-secondary
@@ -20,17 +75,45 @@ export const NewInterview = () => {
             <h2 className="font-bold text-lg text-center">+ Add New</h2>
         </div>
         <Dialog open={OpenDialog}>
-        <DialogContent>
+        <DialogContent className="max-w-2xl">
             <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle className="text-2xl">
+                Tell us more about the job you are interviewing</DialogTitle>
             <DialogDescription>
-                This action cannot be undone. This will permanently delete your account
-                and remove your data from our servers.
-                <div className="flex gap-5 justify-end">
-                    <Button variant = "ghost" 
-                    onClick={()=>setOpenDialog(false)}>Cancel</Button>
-                    <Button>Start Interview</Button>
+                <form onSubmit={onSubmit}>
+                <div>
+                    <h2>
+                        Add details about your job position/role, job description,
+                        your skills and years of experience 
+                    </h2>
+                    <div className="my-3 p-1">
+                        <label>Job Role/Job Position</label>
+                        <Input placeholder="Eg. FrontEnd Engineer" required
+                        onChange={(e)=>setJobPosition(e.target.value)}></Input>
+                    </div>
+                    <div className="my-1 p-1">
+                        <label>Job Description and Tech Stack (in short)</label>
+                        <Textarea 
+                        placeholder="Eg. Reactjs, Nodejs, Nextjs, UI design" required
+                        onChange={(e)=>setJobDesc(e.target.value)}></Textarea>
+                    </div>
+                    <div className="my-1 p-1">
+                        <label>Years of Experience</label>
+                        <Input placeholder="Eg. 5" type="number" required max="50"
+                        onChange={(e)=>setJobExperience(e.target.value)}></Input>
+                    </div>
                 </div>
+                <div className="flex gap-5 justify-end">
+                    <Button variant = "ghost" type="button" className="border-2 border-black text-black"
+                    onClick={()=>setOpenDialog(false)}>Cancel</Button>
+                    <Button type="submit" disabled={loadings}>
+                    {loadings ?
+                        <>
+                        <LoaderCircle className = "animate-spin"/>AI is generating questions</>
+                        :"Start Interview"
+                    }</Button>
+                </div>
+                </form>
             </DialogDescription>
             </DialogHeader>
         </DialogContent>
--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
-const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_UR);
+const sql = neon("postgresql://InterviewX_owner:2YFJqk9exwvV@ep-dark-sound-a5kp0959.us-east-2.aws.neon.tech/InterviewX?sslmode=require");
 export const db = drizzle(sql, {schema});
