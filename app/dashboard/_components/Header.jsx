70bd407a06f7d993f8265b38848a21cc9502fa18
AadityaTripathi <tripathiaaditya02@gmail.com>
2024-08-01 04:18:16 +0530

    added previous interview cards

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -2,14 +2,19 @@
 import React from 'react'
 import Image from 'next/image'
 import { UserButton } from '@clerk/nextjs'
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 function Header() {
     
     const path = usePathname();
-  
+    const router = useRouter();
+
+    const BackToHome = () => {
+        router.push('/dashboard');
+    }
+
     return (
-    <div className = "flex p-4 items-center justify-between bg-secondary shadow-sm">
-        <div class = "flex flex-row">
+    <div className = "flex p-4 items-center justify-between bg-secondary shadow-sm cursor-pointer">
+        <div class = "flex flex-row" onClick={BackToHome}>
             <Image src = {'/logo.svg'} width={100} height={85} alt='logo'></Image>
             <div class="text-2xl p-4 font-bold">InterviewX</div>
         </div>
